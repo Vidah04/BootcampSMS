b423cc42f7cb2d4c695f90ce3e6adf32588d60f7
Wahidatin HR <wahrahmah496@gmail.com>
2019-03-11 11:04:07 +0700

    firstpost

---
+++ BootcampSMS/BootcampSMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BootcampSMS.Startup))]
+namespace BootcampSMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
